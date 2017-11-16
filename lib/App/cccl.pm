7bb50de0ebba505122df014aee5f637a8c17c001
Graham Ollis <plicease@cpan.org>
2017-11-16 10:06:17 -0500

    initial structure

--- lib/App/cccl.pm
+++ lib/App/cccl.pm
@@ -1,10 +1,75 @@
-package App::cccl;
+package App::cccl {
 
-use strict;
-use warnings;
-use 5.008001;
+  use strict;
+  use warnings;
+  use 5.026;
+  use experimental qw( signatures );
 
-# ABSTRACT: Wrapper for cl to make it work like cc
-# VERSION
+  # ABSTRACT: Wrapper for cl to make it work like cc
+  # VERSION
+  
+  sub main ($, @args)
+  {
+    my $cl = App::cccl::cl->new;
+  
+    foreach my $arg (@args)
+    {
+      if($arg =~ /^-/)
+      {
+        # treat as option
+        if($arg eq '--version')
+        {
+          say STDERR "App::cccl version @{[ $App::cccl::VERSION // 'dev' ]}";
+          say STDERR $cl->about;
+          return 0;
+        }
+        else
+        {
+          say STDERR "unknown option: $arg";
+          return 1;
+        }
+      }
+      else
+      {
+        # treat as file
+      }
+    }
+  
+    return 0;
+  }
+
+}
+
+package App::cccl::cl {
+
+  use Moo;
+  use 5.026;
+  use experimental qw( signatures );
+  use File::Which qw( which );
+  use Capture::Tiny qw( capture );
+  
+  has exe => (
+    is      => 'ro',
+    default => sub {
+      which($ENV{CCCL_CL} // 'cl');
+    },
+  );
+  
+  sub about ($self)
+  {
+    if($self->exe)
+    {
+      my(undef, $stderr) = capture { system $self->Exe }; 
+      return 
+        "CL=@{[ $self->exe ]}\n" .
+        "$stderr";
+    }
+    else
+    {
+      return 'CL not found.';
+    }
+  }
+
+}
 
 1;
